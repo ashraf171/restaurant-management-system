99fda93b09323042483179050313b20a18ca6c21
Ashraf almouaie <ashrafalmouaie>
2026-01-10 13:06:00 -0800

    Merge backup of Suad's changes

--- customers/models.py
+++ customers/models.py
@@ -4,6 +4,10 @@ from django.core.validators import RegexValidator
 # Create your models here.
 
 class Customer(models.Model):
+    phone_regex = RegexValidator(
+    regex=r'^\+?1?\d{9,15}$',
+    message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
+    )
     first_name = models.CharField(max_length=60, db_index=True)
     last_name = models.CharField(max_length=60, db_index=True)
     email = models.EmailField()
