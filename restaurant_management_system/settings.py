f21e5df092a0efa9a62563ec6b0f0f10beeb4bd1
Ashraf almouaie <ashrafalmouaie>
2026-01-10 09:16:46 -0800

    Update REST_FRAMEWORK settings: JWT authentication and pagination

--- restaurant_management_system/settings.py
+++ restaurant_management_system/settings.py
@@ -48,7 +48,10 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
 }
 
 #AUTH_USER_MODEL = 'users.User'
