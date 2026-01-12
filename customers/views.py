ed31abaf2d8f8e9e085ce71a9650b79754f60cb4
souadroumani <soaadromani99@gmail.com>
2026-01-12 21:53:51 +0300

    Update customers/views.py and get permissions.py from nagham branch

--- customers/views.py
+++ customers/views.py
@@ -1,14 +1,22 @@
-from rest_framework.viewsets import ModelViewSet
+
 from .models import Customer
 from .serializer import CustomerSerializer
+
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
-from .permissions import CustomerPermission
-
-
+from restaurant_management_system.permissions import IsAdmin, IsManagerOrAdmin
 # Create your views here.
 
 class CustomerViewSet(ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
-    permission_classes = [IsAuthenticated, CustomerPermission]
 
+    def get_permissions(self):
+        # GET for admin , manager
+        if self.request.method in ['GET', 'HEAD', 'OPTIONS']:
+            permission_classes = [IsAuthenticated, IsManagerOrAdmin]
+        else:
+            # POST, PUT, PATCH, DELETE  for Admin
+            permission_classes = [IsAuthenticated, IsAdmin]
+
+        return [permission() for permission in permission_classes]
