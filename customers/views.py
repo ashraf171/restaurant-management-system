f6df056f6371d02f3cf8940862de09e0fa076291
souadroumani <soaadromani99@gmail.com>
2026-01-12 19:18:33 +0300

    Update customer permissions and views

--- customers/views.py
+++ customers/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Customer
 from .serializer import CustomerSerializer
+from rest_framework.permissions import IsAuthenticated
+from .permissions import CustomerPermission
 
 
 # Create your views here.
@@ -8,3 +10,5 @@ from .serializer import CustomerSerializer
 class CustomerViewSet(ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [IsAuthenticated, CustomerPermission]
+
